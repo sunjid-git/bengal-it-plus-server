ac6ae5240dc3e5fd766b1ac3db3191a9f27b5ed9
sunjid-git <sunjid.info@gmail.com>
2021-04-20 12:18:38 +0600

    Root URL

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('root api!')
+  res.send('Bengal It Plus!')
 })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vqs6b.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;

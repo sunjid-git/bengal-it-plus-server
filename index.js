e8268bd3c87a077d1d92e9f0bb7a6fdb4b1962f2
sunjid-git <sunjid.info@gmail.com>
2021-04-20 12:16:26 +0600

    heroku url

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('root api!')
 })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vqs6b.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
